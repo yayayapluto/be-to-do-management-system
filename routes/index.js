b5b665811edcc7ccf53f1c1d65159ea0e5bcc68f
yyypluto <mfarrasashshiddiqi@gmail.com>
2025-01-25 11:38:05 +0700

    docs: api docs via postman

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+const { default: apiResponse } = require('../utils/api-response');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.status(200).json(apiResponse(true, "Message here", "Datas(s) here, can be any data types"))
 });
 
 module.exports = router;
