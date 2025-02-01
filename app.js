3f2f59fefb98d2f9d4ff094a7f693596efedd146
yyypluto <mfarrasashshiddiqi@gmail.com>
2025-02-01 14:20:04 +0700

    Test Commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,10 +8,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const { configDotenv } = require('dotenv');
 
 var app = express();
-configDotenv()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,12 +26,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -40,6 +40,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 
 module.exports = app;
--- utils/api-response.js
+++
@@ -1,9 +0,0 @@
-export default function apiResponse(success, message, data) {
-    return {
-        status: {
-            success: success,
-            message: message || ""
-        },
-        data: data || null
-    }
-}
